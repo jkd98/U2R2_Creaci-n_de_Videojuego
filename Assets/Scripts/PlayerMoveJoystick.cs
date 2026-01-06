b180d287a74b2439e7bb773304b639ebc7b469f0
jk <josesitoelalto@gmail.com>
2026-01-05 20:46:46 -0600

    Leccion 14: joystick

---
+++ Assets/Scripts/PlayerMoveJoystick.cs
@@ -0,0 +1,97 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMoveJoystick : MonoBehaviour
+{
+
+    //referencia al joystick
+    private float horizontalMove = 0f;
+    private float verticalMove = 0f;
+    public Joystick joystick;
+
+    public float runSpeed = 1.5f;
+    public float jumpSpeed = 3.0f;
+    Rigidbody2D rb2d; // referencia al rigidbody
+
+
+    // Para animaciones
+    public SpriteRenderer spriteRenderer; // referencia al sprite renderer del componente
+    public Animator animator; // referencia al animator del componente
+
+    // Para doble salto
+    public float doubleJumpSpeed = 2.5f; // Para que el doble salto sea un poco más bajo
+    private bool canDoubleJump = false;
+
+    void Start()
+    {
+        rb2d = GetComponent<Rigidbody2D>(); //obtiene referencia de el componente
+    }
+
+    void Update()
+    {
+        // amimaciones de correr
+        if (horizontalMove > 0.1f)
+        {
+            spriteRenderer.flipX = false; // mirar a la derecha
+            animator.SetBool("Run", true);
+        }
+        else if (horizontalMove < -0.1f)
+        {
+            spriteRenderer.flipX = true; // mirar a la izquierda
+            animator.SetBool("Run", true);
+        }
+        else
+        {
+            animator.SetBool("Run", false);
+        }
+
+        // Animaciones de salto
+        if (CheckGround.isGrounded)
+        {
+            animator.SetBool("Jump", false);
+            animator.SetBool("DoubleJump", false);
+            animator.SetBool("Falling", false);
+
+        }
+        else
+        {
+            animator.SetBool("Jump", true);
+            animator.SetBool("Run", false);
+        }
+
+        if (rb2d.velocity.y < -0.1f)
+        {
+            animator.SetBool("Falling", true);
+        }
+        else if (rb2d.velocity.y > 0.1f)
+        {
+            animator.SetBool("Falling", false);
+        }
+    }
+
+    void FixedUpdate()
+    {
+        horizontalMove = joystick.Horizontal * runSpeed;
+        transform.position += new Vector3(horizontalMove, 0, 0) * Time.deltaTime * runSpeed;
+    }
+
+    public void Jump()
+    {
+        if (CheckGround.isGrounded) // Si está en el suelo
+        {
+            canDoubleJump = true; // Resetea la posibilidad de doble salto 
+            rb2d.velocity = new Vector2(rb2d.velocity.x, jumpSpeed);
+        }
+        else
+        {
+            if (canDoubleJump)
+            {
+                animator.SetBool("DoubleJump", true);
+                rb2d.velocity = new Vector2(rb2d.velocity.x, doubleJumpSpeed);
+                canDoubleJump = false; // Desactiva la posibilidad de otro doble salto
+            }
+        }
+    }
+
+}
--- README.md
+++ README.md
@@ -22,6 +22,21 @@ Este es un repositorio que contiene las lecciones aprendidas de la unidad
 * Documento leccion 11: https://drive.google.com/file/d/1RqXm1Ky8IM8HS8Dk0C41RY9RUxguhPip/view?usp=sharing
 * Documento leccion 12: https://drive.google.com/file/d/1hWwrc3JqSvyPbf02kmxWRH_HLZqKQM6D/view?usp=sharing
 * Documento leccion 13: https://drive.google.com/file/d/1N0jyhLu0qEQ3mVfp9jcO4HJTo4OL7qJH/view?usp=sharing
+* Documento leccion 14: https://drive.google.com/file/d/1M_m2R5XawHO3gU_g3DKp-L-uV_d6T3Q3/view?usp=sharing
+* Documento leccion #: 
+* Documento leccion #: 
+* Documento leccion #: 
+* Documento leccion #: 
+* Documento leccion #: 
+* Documento leccion #: 
+* Documento leccion #: 
+* Documento leccion #: 
+* Documento leccion #: 
+* Documento leccion #: 
+* Documento leccion #: 
+* Documento leccion #: 
+* Documento leccion #: 
+* Documento leccion #: 
 * Documento leccion #: 
 * Documento leccion #: 
 ## Videos explicativos de evidencia
@@ -38,6 +53,19 @@ Este es un repositorio que contiene las lecciones aprendidas de la unidad
 * Video leccion 11: https://drive.google.com/file/d/1SVABrY7Z560jvDSltBRTxzzD180i6kUq/view?usp=sharing
 * Video leccion 12: https://drive.google.com/file/d/1ByKI2M227C5ul7Hn5pwn1odK6fLESsPP/view?usp=sharing
 * Video leccion 13: https://drive.google.com/file/d/1jrNsGdok5UMjhwg1Acjm6bcld1wbc5lR/view?usp=sharing
+* Video leccion 14: https://drive.google.com/file/d/1_TCzaB4bfU8FvXoLPRZc7YIPI4lbpHfJ/view?usp=sharing
+* Video leccion #: 
+* Video leccion #: 
+* Video leccion #: 
+* Video leccion #: 
+* Video leccion #: 
+* Video leccion #: 
+* Video leccion #: 
+* Video leccion #: 
+* Video leccion #: 
+* Video leccion #: 
+* Video leccion #: 
+* Video leccion #: 
 * Video leccion #: 
 * Video leccion #: 
 * Video leccion #: 
